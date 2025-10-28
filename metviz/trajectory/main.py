2276c5b9d93773cece928787ec7e8ea3733be378
Massimo Di Stefano <epiesasha@me.com>
2025-10-28 16:21:35 +0100

    Update copyright year to 2025

--- metviz/trajectory/main.py
+++ metviz/trajectory/main.py
@@ -1,7 +1,7 @@
 """
 ====================
 
-Copyright 2022 MET Norway
+Copyright 2025 MET Norway
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
