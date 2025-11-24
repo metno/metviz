d3e52f5d727d184a77c1e38636e9f655392c7d6a
epifanio <epiesasha@me.com>
2025-11-24 13:44:13 +0000

    adding docker compose

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,5 @@ LOG_LEVEL=${LOG_LEVEL:-debug}
 ## light #beb9d7   
 ## dark #464769 
 
-panel serve --port ${PORT} --basic-auth /credentials.json --cookie-secret my_super_safe_cookie_secret --autoreload --address 0.0.0.0 --log-level ${LOG_LEVEL} --index /assets/custom_index.html --static-dirs assets=/assets --allow-websocket-origin="ows.terrestris.de" /trajectory /tspt /OGC_client /seaice/daily /seaice/monthly --index-titles /map="Trajectory widget" /tspt="Time Series Profile Tool" /csw="OGC Client" /daily="Sea Ice Daily" /monthly="Sea Ice Monthly"
+# --basic-auth /credentials.json
+panel serve --port ${PORT} --cookie-secret my_super_safe_cookie_secret --autoreload --address 0.0.0.0 --log-level ${LOG_LEVEL} --index /assets/custom_index.html --static-dirs assets=/assets --allow-websocket-origin='*'  /trj /mapdap /trajectory /tspt /OGC_client /seaice/daily /seaice/monthly /seaicemod/monthlymod  --index-titles /trj="trj" /mapdap="Trajectory Loader" /map="Trajectory widget" /tspt="Time Series Profile Tool" /csw="OGC Client" /daily="Sea Ice Daily" /monthly="Sea Ice Monthly" /monthlymod="Sea Ice Model"
