b44963ca24fca24dfbe747fc960956dd422136f1
epifanio <epiesasha@me.com>
2026-01-19 08:42:28 +0000

    remove unused code

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,4 +9,5 @@ LOG_LEVEL=${LOG_LEVEL:-debug}
 # /mapdap /mapdap="Trajectory Loader"
 # /map="Trajectory widget"
 # /trajectory
-panel serve --port ${PORT} --cookie-secret my_super_safe_cookie_secret --autoreload --address 0.0.0.0 --log-level ${LOG_LEVEL} --index /assets/custom_index.html --static-dirs assets=/assets --allow-websocket-origin='*'  /TSP /trj /tspt /OGC_client /seaice/daily /seaice/monthly /seaicemod/monthlymod  --index-titles /TSP="TSP" /trj="trj" /tspt="Time Series Profile Tool" /csw="OGC Client" /daily="Sea Ice Daily" /monthly="Sea Ice Monthly" /monthlymod="Sea Ice Model"
+# /tspt
+panel serve --port ${PORT} --cookie-secret my_super_safe_cookie_secret --autoreload --address 0.0.0.0 --log-level ${LOG_LEVEL} --index /assets/custom_index.html --static-dirs assets=/assets --allow-websocket-origin='*'  /TSP /trj /OGC_client /seaice/daily /seaice/monthly /seaicemod/monthlymod  --index-titles /TSP="NC-Visualization Tool" /trj="trj" /csw="OGC Client" /daily="Sea Ice Daily" /monthly="Sea Ice Monthly" /monthlymod="Sea Ice Model"
